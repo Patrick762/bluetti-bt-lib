ce28f466878adce5cd318f0131528d8582b96491
mikwhe <mikesunmed@gmail.com>
2026-08-11 22:18:15 +0100

    EP760: fix AC_P1_POWER signed wraparound, add confirmed registers

--- bluetti_bt_lib/devices/ep760.py
+++ bluetti_bt_lib/devices/ep760.py
@@ -1,5 +1,5 @@
 from ..base_devices import BaseDeviceV2
-from ..fields import FieldName, UIntField, DecimalField, SwitchField
+from ..fields import FieldName, UIntField, DecimalField, SwitchField, IntField
 
 class EP760(BaseDeviceV2):
     def __init__(self):
@@ -11,8 +11,6 @@ class EP760(BaseDeviceV2):
                 UIntField(FieldName.PV_S2_POWER, 1220),
                 DecimalField(FieldName.PV_S2_VOLTAGE, 1221, 1),
                 DecimalField(FieldName.PV_S2_CURRENT, 1222, 1),
-                # Reclassified from SM_P1 (smart meter) - confirmed against
-                # live hardware to be PV String 3, not a smart meter channel.
                 UIntField(FieldName.PV_S3_POWER, 1228),
                 DecimalField(FieldName.PV_S3_VOLTAGE, 1229, 1),
                 DecimalField(FieldName.PV_S3_CURRENT, 1230, 1),
@@ -21,15 +19,13 @@ class EP760(BaseDeviceV2):
                 DecimalField(FieldName.GRID_P1_VOLTAGE, 1314, 1),
                 DecimalField(FieldName.GRID_P1_CURRENT, 1315, 1),
                 DecimalField(FieldName.AC_OUTPUT_FREQUENCY, 1500, 1),
-                UIntField(FieldName.AC_P1_POWER, 1510),
+                IntField(FieldName.AC_P1_POWER, 1510),
                 DecimalField(FieldName.AC_P1_VOLTAGE, 1511, 1),
                 DecimalField(FieldName.AC_P1_CURRENT, 1512, 1),
                 SwitchField(FieldName.CTRL_AC, 2208),
-                # Low/High SOC display - register matches EP600, EP2000, AC300,
-                # AC500, EP500, and your old library's commented mapping.
                 UIntField(FieldName.BATTERY_SOC_RANGE_START, 2022),
                 UIntField(FieldName.BATTERY_SOC_RANGE_END, 2023),
-                # --- Added: HA Energy dashboard totals (from old library) ---
+                UIntField(FieldName.HOUSE_LOAD_POWER, 142,1),
                 UIntField(FieldName.DC_INPUT_POWER, 144,1),
                 DecimalField(FieldName.TOTAL_LOAD_CONSUMPTION, 152, 1),
                 DecimalField(FieldName.POWER_GENERATION, 154, 1),
@@ -37,5 +33,7 @@ class EP760(BaseDeviceV2):
                 DecimalField(FieldName.TOTAL_GRID_FEED, 158, 1),
                 SwitchField(FieldName.CTRL_WORKING_MODE, 2029),
                 SwitchField(FieldName.CTRL_CHARGE_FROM_GRID, 2207),
+                UIntField(FieldName.CTRL_GRID_MAX_IMPORT_POWER, 2213),
+                UIntField(FieldName.CTRL_GRID_MAX_EXPORT_POWER, 2215),
            ],
         )
