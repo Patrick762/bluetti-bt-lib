9389d2e279b6252140ca4f2508427cd5e15a87cd
Sideeffect <sidieje@gmail.com>
2026-08-12 16:55:23 +0100

    Added Charger 2 Device type and serial number, Battery SOC reading is now Power station SOC

--- bluetti_bt_lib/devices/charger2.py
+++ bluetti_bt_lib/devices/charger2.py
@@ -1,5 +1,5 @@
 from ..base_devices import BaseDeviceV2
-from ..fields import FieldName, UIntField, DecimalField, SignedDecimalField, AbsoluteSignedDecimalField
+from ..fields import FieldName, UIntField, DecimalField, SwapStringField, SerialNumberField, SignedDecimalField, AbsoluteSignedDecimalField
 
 
 class CHARGER2(BaseDeviceV2):
@@ -13,10 +13,13 @@ class CHARGER2(BaseDeviceV2):
                 AbsoluteSignedDecimalField(FieldName.PPS_OUTPUT_CURRENT, 15536, 2),
                 AbsoluteSignedDecimalField(FieldName.PPS_OUTPUT_POWER, 15538, 0),
                 DecimalField(FieldName.PS_BATTERY_VOLTAGE, 15543, 1),
-                DecimalField(FieldName.PS_BATTERY_IO_POWER, 15546, 0),
+                AbsoluteSignedDecimalField(FieldName.PS_BATTERY_IO_POWER, 15546, 0),
                 UIntField(FieldName.PS_BATTERY_SOC, 15584),
+                UIntField(FieldName.BATTERY_SOC, 15584),
                 DecimalField(FieldName.BATTERY_VOLTAGE, 15539, 1),
                 DecimalField(FieldName.BATTERY_CURRENT, 15540, 2),
                 DecimalField(FieldName.BATTERY_IO_POWER, 15542, 0),
+                SwapStringField(FieldName.DEVICE_TYPE, 15500, 6),
+                SerialNumberField(FieldName.DEVICE_SN, 15506),
             ],
         )
